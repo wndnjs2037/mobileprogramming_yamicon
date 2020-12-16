a7fb96179d35a6c47c0a1568f9b99a94e7d2ffc9
19)민원희 <52127527+alsdnjsgml@users.noreply.github.com>
2020-12-16 14:19:58 +0900

    합침

--- YummyCon/app/src/main/java/com/example/yummycon/ui/notifications/NotificationsFragment.java
+++ YummyCon/app/src/main/java/com/example/yummycon/ui/notifications/NotificationsFragment.java
@@ -13,23 +13,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.yummycon.R;
+import com.journeyapps.barcodescanner.CaptureManager;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private CaptureManager captur;
+    private DecoratedBarcodeView barcodeScannerView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_notifications);
     }
 }
